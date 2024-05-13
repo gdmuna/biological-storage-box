31e03d7dd761e1ba75ae2f3d432322f77af3941c
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-05-13 18:12:31 +0800

    import mdi icons

--- biological-storage-box-web/src/main.js
+++ biological-storage-box-web/src/main.js
@@ -14,6 +14,7 @@ import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 const vuetify = createVuetify({
     components,
     directives,
