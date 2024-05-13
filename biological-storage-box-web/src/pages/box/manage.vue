de7ac1318b33b4daabbaea16fc8656533e2acead
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-05-13 23:23:55 +0800

    modified the page style of the box manage page

--- biological-storage-box-web/src/pages/box/manage.vue
+++ biological-storage-box-web/src/pages/box/manage.vue
@@ -5,14 +5,26 @@
                 <v-card>
                     <v-card-item>
                         <v-card-title>{{ item.name }}</v-card-title>
-                        <v-card-subtitle>{{ item.createBy }}</v-card-subtitle>
+                        <v-card-subtitle>
+                            创建者：
+                            <v-chip size="small" color="indigo">
+                                <v-icon icon="mdi-account-circle" start></v-icon>
+                                {{ item.createBy }}
+                            </v-chip>
+                        </v-card-subtitle>
                     </v-card-item>
-                    <v-card-text>{{ item.createTime }}</v-card-text>
-                    <v-card-text>{{ item.updateTime }}</v-card-text>
+                    <v-card-text class="py-1">
+                        创建时间：
+                        <v-chip size="small" color="deep-purple-lighten-1">{{ item.createTime }}</v-chip>
+                    </v-card-text>
+                    <v-card-text class="py-1">
+                        更新时间：
+                        <v-chip size="small" color="purple-lighten-1">{{ item.updateTime }}</v-chip>
+                    </v-card-text>
                     <v-card-actions>
-                        <v-btn variant="flat" color="light-green-lighten-4" text>查看</v-btn>
-                        <v-btn variant="flat" color="light-green-lighten-4" text>编辑</v-btn>
-                        <v-btn variant="flat" color="light-green-lighten-4" text>删除</v-btn>
+                        <v-btn variant="flat" color="light-green-lighten-4" @click="routeToManageReagent(item.id)">管理试剂</v-btn>
+                        <v-btn variant="flat" color="light-green-lighten-4">编辑信息</v-btn>
+                        <v-btn variant="flat" color="light-green-lighten-4">删除</v-btn>
                     </v-card-actions>
                 </v-card>
             </div>
@@ -35,9 +47,14 @@ export default {
     },
     updated() {},
     methods: {
+        // 获取试剂盒列表
         async getBox() {
             const result = await this.$api.box.org({ pageNum: 1, pageSize: 10 });
             this.boxList = result;
+        },
+        // 跳转到管理试剂页面
+        async routeToManageReagent(boxId) {
+            this.$router.push({ path: '/reagent', query: { boxId } });
         }
     }
 };
