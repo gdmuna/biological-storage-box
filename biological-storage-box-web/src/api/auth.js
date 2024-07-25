5d241b38bad75a270786752104c36d597212c3c4
csilenti <3108006259@qq.com>
2024-07-25 16:25:05 +0800

    注册页面

--- biological-storage-box-web/src/api/auth.js
+++ biological-storage-box-web/src/api/auth.js
@@ -1,9 +1,15 @@
 import authentication from '@/utils/authentication';
 
+import request from '@/utils/request';
+
 const auth = {
     // 用户登录
     login(data) {
         return authentication.login(data);
+    },
+    // 用户注册
+    register(data) {
+        return request.post(`/user/register`, data);
     }
 };
 
