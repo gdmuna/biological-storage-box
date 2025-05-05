61a8e3d6022275a714466300220c85f88abafdc4
csilenti <147641029+csilenti@users.noreply.github.com>
2025-05-06 00:18:23 +0800

    perf: 增加接口

--- biological-storage-box-web/src/api/user.js
+++ biological-storage-box-web/src/api/user.js
@@ -3,6 +3,14 @@ import request from '@/utils/request';
 const moduleUrl = '/user';
 
 const user = {
+    // 向用户邮箱发送验证码
+    sendEmail(data) {
+        return request.get(`${moduleUrl}/email/code`, data);
+    },
+    // 通过邮箱验证码修改密码
+    updatePasswordByCode(data) {
+        return request.put(`${moduleUrl}/email/update/password`, data);
+    },
     // 查询用户个人信息
     userInfo(data) {
         return request.get(`${moduleUrl}/info`, data);
@@ -11,18 +19,20 @@ const user = {
     search(data) {
         return request.get(`${moduleUrl}/search`, data);
     },
+    //修改邮箱
+    updateEmail(data) {
+        return request.put(`${moduleUrl}/update/email`, data);
+    },
     // 修改用户个人信息
     updateInfo(data) {
         return request.put(`${moduleUrl}/update/info`, data);
     },
-    // 修改用户密码
+    // 修改密码
     updatePassword(data) {
         return request.put(`${moduleUrl}/update/password`, data);
     },
-    // 向用户邮箱发送验证码
-    sendEmail(data) {
-        return request.post(`${moduleUrl}/test/code`, data);
-    }
+    
+    
 };
 
 export default user;
