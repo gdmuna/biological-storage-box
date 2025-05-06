d659a558edd9c4a3dbdb749dfbdca4f751087d84
csilenti <147641029+csilenti@users.noreply.github.com>
2025-05-06 11:26:29 +0800

    feat：新增邮箱登录页面（还未完全可使用）

--- biological-storage-box-web/src/router/index.js
+++ biological-storage-box-web/src/router/index.js
@@ -15,6 +15,8 @@ const routes = [
             showNavBar: true
         }
     },
+
+    // 登录页面录路由
     {
         path: '/auth/login',
         name: 'login',
@@ -24,76 +26,38 @@ const routes = [
         }
     },
     {
-        path: '/box',
-        name: 'box-manage',
-        component: () => import('@/pages/box/manage.vue'),
+        path: '/auth/codeLogin',
+        name: 'codeLogin',
+        component: () => import('@/pages/auth/codeLogin.vue'),
         meta: {
-            title: '试剂盒管理',
+            title: '邮箱验证码登录'
+        }
+    },
+    {
+        path: '/auth/register',
+        name: 'register',
+        component: () => import('@/pages/auth/register.vue'),
+        meta: {
+            title: '注册页面'
+        }
+    },
+    {
+        path: '/auth/resetPassword',
+        name: 'resetPassword',
+        component: () => import('@/pages/auth/resetPassword.vue'),
+        meta: {
+            title: '重置密码'
+        }
+    },
+
+    // 底部导航栏路由
+    {
+        path: '/grid',
+        name: 'work',
+        component: () => import('@/pages/work/work.vue'),
+        meta: {
+            title: '工作台',
             showNavBar: true
-        }
-    },
-    {
-        path: '/box/create',
-        name: 'box-create',
-        component: () => import('@/pages/box/create.vue'),
-        meta: {
-            title: '试剂盒创建'
-        }
-    },
-    {
-        path: '/box/update',
-        name: 'box-update',
-        component: () => import('@/pages/box/update.vue'),
-        meta: {
-            title: '更改试剂盒信息'
-        }
-    },
-    {
-        path: '/reagent',
-        name: 'reagent-manage',
-        component: () => import('@/pages/reagent/manage.vue'),
-        meta: {
-            title: '试剂管理'
-        }
-    },
-    {
-        path: '/reagent/operate',
-        name: 'reagent-operate',
-        component: () => import('@/pages/reagent/operate.vue'),
-        meta: {
-            title: '试剂操作'
-        }
-    },
-    {
-        path: '/reagent/upload',
-        name: 'reagent-upload',
-        component: () => import('@/pages/reagent/upload.vue'),
-        meta: {
-            title: '图片上传'
-        }
-    },
-    {
-        path: '/reagent/log',
-        name: 'reagent-log',
-        component: () => import('@/pages/reagent/log.vue'),
-        meta: {
-            title: '日志'
-        }
-    },
-    {
-        path: '/reagent/detail',
-        name: 'reagent-detail',
-        component: () => import('@/pages/reagent/detail.vue'),
-        meta: {
-            title: '试剂详情'
-        }
-    },
-    {
-        path: '/box/detail',
-        name: 'box-detail',
-        component: () => import('@/pages/box/detail.vue'),
-        meta: {
-            title: '试剂盒详情'
         }
     },
     {
@@ -105,63 +69,7 @@ const routes = [
             showNavBar: true
         }
     },
-    {
-        path: '/grid',
-        name: 'work',
-        component: () => import('@/pages/work/work.vue'),
-        meta: {
-            title: '工作台',
-            showNavBar: true
-        }
-    },
-    {
-        path: '/org/create',
-        name: 'org-create',
-        component: () => import('@/pages/org/create.vue'),
-        meta: {
-            title: '课题组创建'
-        }
-    },
-    {
-        path: '/org/exit',
-        name: 'org-exit',
-        component: () => import('@/pages/org/exit.vue'),
-        meta: {
-            title: '课题组退出或编辑'
-        }
-    },
-    {
-        path: '/org/update',
-        name: 'org-update',
-        component: () => import('@/pages/org/update.vue'),
-        meta: {
-            title: '课题组编辑'
-        }
-    },
-    {
-        path: '/search/search',
-        name: 'search',
-        component: () => import('@/pages/search/search.vue'),
-        meta: {
-            title: '搜索'
-        }
-    },
-    {
-        path: '/org/joinOrg',
-        name: 'joinOrg',
-        component: () => import('@/pages/org/joinOrg.vue'),
-        meta: {
-            title: '搜索加入组织'
-        }
-    },
-    {
-        path: '/org/applyReason',
-        name: 'applyReason',
-        component: () => import('@/pages/org/applyReason.vue'),
-        meta: {
-            title: '申请理由'
-        }
-    },
+    // 用户信息路由
     {
         path: '/user/setting',
         name: 'setting',
@@ -202,12 +110,13 @@ const routes = [
             title: '审核被邀请加入组织的消息'
         }
     },
+    // 工具台路由
     {
-        path: '/auth/register',
-        name: 'register',
-        component: () => import('@/pages/auth/register.vue'),
+        path: '/search/search',
+        name: 'search',
+        component: () => import('@/pages/search/search.vue'),
         meta: {
-            title: '注册页面'
+            title: '搜索'
         }
     },
     {
@@ -219,6 +128,50 @@ const routes = [
         }
     },
     {
+        path: '/org/joinOrg',
+        name: 'joinOrg',
+        component: () => import('@/pages/org/joinOrg.vue'),
+        meta: {
+            title: '搜索加入组织'
+        }
+    },
+    {
+        path: '/org/applyReason',
+        name: 'applyReason',
+        component: () => import('@/pages/org/applyReason.vue'),
+        meta: {
+            title: '申请理由'
+        }
+    },
+    
+    //课题组层级路由
+    {
+        path: '/org/create',
+        name: 'org-create',
+        component: () => import('@/pages/org/create.vue'),
+        meta: {
+            title: '课题组创建'
+        }
+    },
+    {
+        path: '/org/exit',
+        name: 'org-exit',
+        component: () => import('@/pages/org/exit.vue'),
+        meta: {
+            title: '课题组退出或编辑'
+        }
+    },
+    {
+        path: '/org/update',
+        name: 'org-update',
+        component: () => import('@/pages/org/update.vue'),
+        meta: {
+            title: '课题组编辑'
+        }
+    },
+
+    //房间层级路由
+    {
         path: '/root/manageRoot',
         name: 'manageRoot',
         component: () => import('@/pages/root/manageRoot.vue'),
@@ -227,14 +180,7 @@ const routes = [
             showNavBar: true
         }
     },
-    {
-        path: '/auth/resetPassword',
-        name: 'resetPassword',
-        component: () => import('@/pages/auth/resetPassword.vue'),
-        meta: {
-            title: '重置密码'
-        }
-    },
+    
     {
         path: '/root/updateRoot',
         name: 'updateRoot',
@@ -250,7 +196,84 @@ const routes = [
         meta: {
             title: '创建房间'
         }
-    }
+    },
+
+    //试剂盒层级路由
+    {
+        path: '/box/detail',
+        name: 'box-detail',
+        component: () => import('@/pages/box/detail.vue'),
+        meta: {
+            title: '试剂盒详情'
+        }
+    },
+    {
+        path: '/box',
+        name: 'box-manage',
+        component: () => import('@/pages/box/manage.vue'),
+        meta: {
+            title: '试剂盒管理',
+            showNavBar: true
+        }
+    },
+    {
+        path: '/box/create',
+        name: 'box-create',
+        component: () => import('@/pages/box/create.vue'),
+        meta: {
+            title: '试剂盒创建'
+        }
+    },
+    {
+        path: '/box/update',
+        name: 'box-update',
+        component: () => import('@/pages/box/update.vue'),
+        meta: {
+            title: '更改试剂盒信息'
+        }
+    },
+
+    //试剂层级路由
+    {
+        path: '/reagent/detail',
+        name: 'reagent-detail',
+        component: () => import('@/pages/reagent/detail.vue'),
+        meta: {
+            title: '试剂详情'
+        }
+    },
+    {
+        path: '/reagent',
+        name: 'reagent-manage',
+        component: () => import('@/pages/reagent/manage.vue'),
+        meta: {
+            title: '试剂管理'
+        }
+    },
+    {
+        path: '/reagent/operate',
+        name: 'reagent-operate',
+        component: () => import('@/pages/reagent/operate.vue'),
+        meta: {
+            title: '试剂操作'
+        }
+    },
+    {
+        path: '/reagent/upload',
+        name: 'reagent-upload',
+        component: () => import('@/pages/reagent/upload.vue'),
+        meta: {
+            title: '图片上传'
+        }
+    },
+    {
+        path: '/reagent/log',
+        name: 'reagent-log',
+        component: () => import('@/pages/reagent/log.vue'),
+        meta: {
+            title: '日志'
+        }
+    },
 ];
 
 const router = createRouter({
